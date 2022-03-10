2bdca0e9087615aa57beebce950052fe7189a9e0
0xClandestine <Caveman3333@protonmail.com>
2022-03-10 04:12:51 -0600

    ♻️ Removed unnecessary return statements,
    - bit of syntax cleanup as well, prob a small amount of gas savings

--- contracts/Uint512.sol
+++ contracts/Uint512.sol
@@ -15,7 +15,6 @@ library Uint512 {
 			r0 := mul(a, b)
 			r1 := sub(sub(mm, r0), lt(mm, r0))
 		}
-		return (r0, r1);
 	}
 
 	/// @notice Calculates the product and remainder of two uint256
@@ -32,7 +31,6 @@ library Uint512 {
 			r1 := sub(sub(mm, r0), lt(mm, r0))
 			r2 := mulmod(a, b, c)
 		}
-		return (r0, r1, r2);
 	}
 
 	/// @notice Calculates the difference of two uint512
@@ -47,7 +45,6 @@ library Uint512 {
 			r0 := add(a0, b0)
 			r1 := add(add(a1, b1), lt(r0, a0))
 		}
-		return (r0, r1);
 	}
 
 	/// @notice Calculates the difference of two uint512
@@ -62,7 +59,6 @@ library Uint512 {
 			r0 := sub(a0, b0)
 			r1 := sub(sub(a1, b1), lt(a0, b0))
 		}
-		return (r0, r1);
 	}
 
 	/// @notice Calculates the division of a 512 bit unsigned integer by a 256 bit integer. It 
@@ -95,10 +91,9 @@ library Uint512 {
             a0 := or(a0, mul(a1, pow))
 		}
 
-		if (a1 == 0) {
-			// if a1 is zero after the shifting, a single word division is sufficient
-			return a0 / b;
-		}
+
+		// if a1 is zero after the shifting, a single word division is sufficient
+		if (a1 == 0) return a0 / b;
 
 		assembly {
 			// Calculate the multiplicative inverse mod 2**256 of b. See the paper for details.
@@ -112,7 +107,6 @@ library Uint512 {
 
             r := mul(a0, inv)
 		}
-		return r;
 	}
 
 	/// @notice Calculates the division of a 512 bit unsigned integer by a 256 bit integer. It 
@@ -149,10 +143,8 @@ library Uint512 {
             a0 := or(a0, mul(a1, pow))
 		}
 
-		if (a1 == 0) {
-			// if a1 is zero after the shifting, a single word division is sufficient
-			return a0 / b;
-		}
+		// if a1 is zero after the shifting, a single word division is sufficient
+		if (a1 == 0) return a0 / b;
 
 		assembly {
 			// Calculate the multiplicative inverse mod 2**256 of b. See the paper for details.
@@ -166,7 +158,6 @@ library Uint512 {
 
             r := mul(a0, inv)
 		}
-		return r;
 	}
 
 	/// @notice Calculates the square root of x, rounding down.
@@ -174,13 +165,15 @@ library Uint512 {
 	/// @param x The uint256 number for which to calculate the square root.
 	/// @return s The square root as an uint256.
 	function sqrt(uint256 x) public pure returns (uint256 s) {
-		if (x == 0) {
-			return 0;
-		}
+		
+		if (x == 0) return 0;
 
-		uint256 xAux = uint256(x);
-		s = 1;
 		assembly {
+
+			s := 1
+
+			let xAux := x
+
 			let cmp := or(gt(xAux, 0x100000000000000000000000000000000), eq(xAux, 0x100000000000000000000000000000000))
 			xAux := sar(mul(cmp, 128), xAux)
 			s := shl(mul(cmp, 64), s)
@@ -227,12 +220,13 @@ library Uint512 {
 	/// @param a1 A uint256 representing the high bits of the input.
 	/// @return s The square root as an uint256. Result has at most 256 bit.
 	function sqrt512(uint256 a0, uint256 a1) public pure returns (uint256 s) {
-		if (a1 == 0) {
-			// A simple 256 bit square root is sufficient
-			return sqrt(a0);
-		}
+		
+		// A simple 256 bit square root is sufficient
+		if (a1 == 0) return sqrt(a0);
+
 		// The used algorithm has the pre-condition a1 >= 2**254
 		uint256 shift;
+
 		assembly {
 			let digits := mul(lt(a1, 0x100000000000000000000000000000000), 128)
 			a1 := shl(digits, a1)
@@ -265,36 +259,42 @@ library Uint512 {
 			a1 := or(a1, shr(sub(256, shift), a0))
 			a0 := shl(shift, a0)
 		}
+		
 		uint256 sp = sqrt(a1);
-        	uint256 rp = a1 - (sp * sp);
-		unchecked {
-			uint256 nom;
-			uint256 denom;
-			uint256 u;
-			uint256 q;
-			assembly {
-				nom := or(shl(128, rp), shr(128, a0))
-				denom := shl(1, sp)
-				q := div(nom, denom)
-				u := mod(nom, denom)
+        uint256 rp = a1 - (sp * sp);
 
-				// The nominator can be bigger than 2**256. We know that rp < (sp+1) * (sp+1). As sp can be
-				// at most floor(sqrt(2**256 - 1)) we can conclude that the nominator has at most 513 bits
-				// set. An expensive 512x256 bit division can be avoided by treating the bit at position 513 manually
-				let carry := shr(128, rp)
-				let x := mul(carry, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
-				q := add(q, div(x, denom))
-				u := add(u, add(carry, mod(x, denom)))
-				q := add(q, div(u, denom))
-				u := mod(u, denom)
-			}
+		uint256 nom;
+		uint256 denom;
+		uint256 u;
+		uint256 q;
+
+		assembly {
+			nom := or(shl(128, rp), shr(128, a0))
+			denom := shl(1, sp)
+			q := div(nom, denom)
+			u := mod(nom, denom)
+
+			// The nominator can be bigger than 2**256. We know that rp < (sp+1) * (sp+1). As sp can be
+			// at most floor(sqrt(2**256 - 1)) we can conclude that the nominator has at most 513 bits
+			// set. An expensive 512x256 bit division can be avoided by treating the bit at position 513 manually
+			let carry := shr(128, rp)
+			let x := mul(carry, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+			q := add(q, div(x, denom))
+			u := add(u, add(carry, mod(x, denom)))
+			q := add(q, div(u, denom))
+			u := mod(u, denom)
+		}
+
+		unchecked {
 			s = (sp << 128) + q;
 
 			uint256 rl = ((u << 128) | (a0 & 0xffffffffffffffffffffffffffffffff));
-			uint256 rr = q*q;
+			uint256 rr = q * q;
+			
 			if ((q >> 128) > (u >> 128) || (((q >> 128) == (u >> 128)) && rl < rr)) {
 				s = s - 1;
 			}
+			
 			return s >> (shift / 2);
 		}
 	}
